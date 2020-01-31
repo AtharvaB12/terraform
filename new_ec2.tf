6bfcbab02825a3e6ec078a92313207a66f778fbd
Atharva Bhardwaj <51410612+AtharvaB12@users.noreply.github.com>
2020-01-31 19:56:35 +0530

    Update new_ec2.tf

--- new_ec2.tf
+++ new_ec2.tf
@@ -5,8 +5,7 @@ resource "aws_instance" "myos_new" {
 	ami = "ami-09d069a04349dc3cb"
 	instance_type = "t2.micro"
         iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
-	#vpc_security_group_ids = ["sg-0c1ef98d1c91b2637"] 
-        aws_sg_attachment = "$(aws_sg_attachment.sg_attachment.name}"
+	vpc_security_group_ids = ["sg-0c1ef98d1c91b2637"] 
 	root_block_device  {
         volume_size = "10"
 }
